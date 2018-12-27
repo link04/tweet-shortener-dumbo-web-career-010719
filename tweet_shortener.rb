308eec2b25a9b5d68efa19820112433cac8133db
Maximo Bautista <max.bautista97@gmail.com>
2018-12-27 15:05:57 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,8 @@
 def dictionar
   shorter_words = {
     "hello" => "hi",
-    "to", two, too" "=> '2' 
+    "to" => '2', , too" "=> '2',
+    "two" => '2'
 "for, four" => '4'
 'be' => 'b'
 'you' => 'u'
