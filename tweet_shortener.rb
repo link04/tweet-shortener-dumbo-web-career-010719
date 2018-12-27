53946536306705d10aa0676d1fe43df3bc9e75f9
Maximo Bautista <max.bautista97@gmail.com>
2018-12-27 15:07:24 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -4,9 +4,10 @@ def dictionar
     "to" => '2', 
     "too" => '2',
     "two" => '2',
-    "for, four" => '4',
-    'be' => 'b',
-    'you' => 'u',
+    "for" => '4',
+    "four" => '4',
+    "be" => 'b',
+    "you" => 'u',
     "at" => "@" ,
     "and" => "&"
   }
