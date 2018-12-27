6e657ad4c450d5ff680461cdd9a2ec32236bbc6c
Maximo Bautista <max.bautista97@gmail.com>
2018-12-27 15:43:46 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -15,7 +15,7 @@ end
 
 def word_substituter(tweet_string)
   tweet_array = tweet_string.split(" ")
-  tweet_array.collect! do |word|
+  tweet_array.collect do |word|
     if dictionary.keys.include? word.downcase
        dictionary.values_at(word.downcase)
     else
@@ -28,19 +28,23 @@ def bulk_tweet_shortener(tweet_array)
   tweet_array.each do |tweet|
     puts word_substituter(tweet)
   end
-
 end
 
-def selective_tweet_shortener(tweet)
-  if tweet.length > 140
-     word_substituter(tweet)
+def selective_tweet_shortener(tweet_string)
+  if tweet_string.length > 140
+     word_substituter(tweet_string)
   else
-    tweet
+    tweet_string
   end
 end
 
-def shortened_tweet_truncator
-  
+def shortened_tweet_truncator(tweet_string)
+  shortened_tweet = word_substituter(tweet_string)
+  if shortened_tweet.length > 140
+    shortened_tweet[0..140]
+  else
+    shortened_tweet
+  end
 end
 
 
